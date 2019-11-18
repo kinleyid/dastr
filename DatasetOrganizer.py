1e4937d654ac3e1f23d13356fd1f70f1ef0e108d
kinleyid <isaac.kinley@gmail.com>
2019-11-17 21:22:46 -0500

    Initial commit

---
+++ DatasetOrganizer.py
@@ -0,0 +1,45 @@
+
+import re, os, shutil
+
+def parse(path, params, master_dict={}):
+	all_entities = [] # That which will be returned
+	curr_param = params[0]
+	if type(curr_param) == str: # We're just going down a level in the file hierarchy
+		all_entities += parse(os.path.join(path, curr_param), params[1:], master_dict=master_dict)
+	else: # We're looping through every file in this level to collect attributes
+		for curr_path_head in os.listdir(path):
+			curr_dict = master_dict.copy()
+			if len(curr_param) == 2: # There are attributes to be added at this level of the hierarchy
+				pattern, curr_attrs = curr_param
+				if type(curr_attrs) != tuple:
+					curr_attrs = (curr_attrs,)
+				matches = re.findall(pattern, curr_path_head)[0]
+				if type(matches) == str:
+					matches = (matches,)
+				for idx in range(len(matches)):
+					curr_dict[curr_attrs[idx]] = matches[idx] # Add attributes
+			curr_path = os.path.join(path, curr_path_head)
+			if os.path.isdir(curr_path): # Recursion if we're currently on a folder
+				all_entities += parse(curr_path, params[1:], master_dict=curr_dict)
+			else: # Bottom of file hierarchy
+				all_entities += [{
+					'path': curr_path,
+					'attrs': curr_dict.copy()
+				}]
+	return all_entities # List of dicts
+
+def write(path, params, all_entities):
+	for entity in all_entities:
+		destination = ''
+		for param in params:
+			if len(param) == 2: # Param is a tuple
+				fmt, args = param
+				if type(args) == str:
+					args = [args]
+				curr_path_head = fmt % tuple([entity['attrs'][attr] for attr in args])
+			else: # Param is a string
+				curr_path_head = param
+			destination = os.path.join(destination, curr_path_head)
+		destination = os.path.join(path, destination)
+		os.makedirs(os.path.dirname(destination), exist_ok=True)
+		shutil.copy(entity['path'], destination)
---
+++ test.py
@@ -0,0 +1,19 @@
+
+import DatasetOrganizer as do
+import os
+
+# From example 1 to example 2
+
+parse_params = [('sub-(\d+)', 'sub'), ('ses-(\d+)', 'ses'), ()]
+example_path = os.path.join(os.getcwd(), 'example1')
+data = do.parse(example_path, parse_params)
+new_path = os.path.join(os.getcwd(), 'example2')
+do.write(new_path, [('sub-%s_ses-%s', ('sub', 'ses')), 'x.txt'], data)
+
+# From example 2 to example 1
+
+parse_params = [('sub-(\d+)_ses-(\d+)', ('sub', 'ses')), ()]
+example_path = os.path.join(os.getcwd(), 'example2')
+data = do.parse(example_path, parse_params)
+new_path = os.path.join(os.getcwd(), 'example1')
+do.write(new_path, [('sub-%s', 'sub'), ('ses-%s', 'ses'), 'x.txt'], data)
